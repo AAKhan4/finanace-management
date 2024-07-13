90f265f610463fcb2c42d260b5c3fe1dbac0feb4
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-14 00:22:04 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const { connectToDatabase, closeDatabaseConnection } = require("./db/conn.mjs");
 
 const app = express();
 const Router = express.Router();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || "";
+
+app.use(async (req, res, next) => {
+  try {
+    const db = await connectToDatabase(MONGO_URI);
+    req.db = db;
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("An error occurred while connecting to the database");
+  }
+});
 
 app.use(express.json());
 Router.route("/")
@@ -15,7 +28,17 @@ Router.route("/")
     res.status(200).send(`Hi ${name}!`);
   });
 
-app.listen(port, (e) => {
-  if (!e) console.log(`Server is running on port ${port}`);
+process.on("SIGINT", async () => {
+  await closeDatabaseConnection();
+  process.exit();
+});
+
+process.on("SIGTERM", async () => {
+  await closeDatabaseConnection();
+  process.exit();
+});
+
+app.listen(PORT, (e) => {
+  if (!e) console.log(`Server is running on port ${PORT}`);
   else console.log(e);
 });
