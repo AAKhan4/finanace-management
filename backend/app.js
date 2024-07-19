794a11fc3e46c3042e6b5f00783d4621a4196f51
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-19 02:15:42 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const { connect, closeConnect } = require("./db/conn.js");
 
 const userRoutes = require("./routes/userRoutes/user");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connect();
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/user", userRoutes);
 
 process.on("SIGINT", async () => {
