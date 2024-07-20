cf2e63640d483ea33e75cdf879ef2d48af53a500
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-20 21:22:54 +0100

    Update index.js

--- frontend/src/components/Signup/index.js
+++ frontend/src/components/Signup/index.js
@@ -8,9 +8,13 @@ export default function Signup() {
                 <E.Form>
                     <E.Title>Sign up</E.Title>
                     <E.Desc>Create a new account.</E.Desc>
+                    <E.Label>Username</E.Label>
                     <E.Input type="text" placeholder="Username" />
-                    <E.Input type="email" placeholder="Email" />
+                    <E.Label>Email</E.Label>
+                    <E.Input type="email" placeholder="email@example.com" />
+                    <E.Label>Password</E.Label>
                     <E.Input type="password" placeholder="Password" />
+                    <E.Label>Confirm Password</E.Label>
                     <E.Input type="password" placeholder="Confirm Password" />
                     <E.Button>Sign up</E.Button>
                 </E.Form>
