23401cd530db03d5a1c7551a21f64dcf124e31c5
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-20 20:11:38 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './utils/Themes';
-import { useState } from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./utils/Themes";
+import { useState } from "react";
 
 function App() {
   const [lightMode, setLightMode] = useState(true);
   return (
-    <ThemeProvider theme={ lightMode? lightTheme : darkTheme }>
+    <ThemeProvider theme={lightMode ? lightTheme : darkTheme}>
       <Router>
         <Routes>
           <Route></Route>
