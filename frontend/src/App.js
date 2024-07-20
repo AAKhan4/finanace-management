3f1779add458c2b6e84e7cf4ee1e29dde1b1fa79
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-20 18:52:48 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './utils/Themes';
+import { useState } from 'react';
 
 function App() {
+  const [lightMode, setLightMode] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={ lightMode? lightTheme : darkTheme }>
+      <Router>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
