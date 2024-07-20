34b552a9b0be67bec6bf03839992cba22e7a3998
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-20 23:56:07 +0100

    Update login

--- frontend/src/components/User/Login/index.js
+++ frontend/src/components/User/Login/index.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as E from "../UserElems";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+require("dotenv").config();
 
-export default function Signup() {
-    return (
-        <E.Container>
-            <E.Wrapper>
-                <E.Form>
-                    <E.Title>Log in</E.Title>
-                    <E.Desc>Sign in to your current account.</E.Desc>
-                    <E.Label>Email</E.Label>
-                    <E.Input type="email" placeholder="email@example.com" />
-                    <E.Label>Password</E.Label>
-                    <E.Input type="password" placeholder="Password" />
-                    <E.Button>Log in</E.Button>
-                </E.Form>
-            </E.Wrapper>
-        </E.Container>
-    );
+export default function Login() {
+  const path = process.env.BACKEND_URL + "/user/login";
+  const form = useRef(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = form.current.elements;
+    const user = {
+      email: data.email.value,
+      password: data.password.value,
+    };
+
+    axios
+      .post(path, user)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <E.Container>
+      <E.Wrapper>
+        <E.Form ref={form} onSubmit={handleSubmit}>
+          <E.Title>Log in</E.Title>
+          <E.Desc>Sign in to your current account.</E.Desc>
+          <E.Label>Email</E.Label>
+          <E.Input type="email" placeholder="email@example.com" name="email" />
+          <E.Label>Password</E.Label>
+          <E.Input type="password" placeholder="Password" name="password" />
+          <E.Button type="submit" >Log in</E.Button>
+        </E.Form>
+      </E.Wrapper>
+    </E.Container>
+  );
 }
