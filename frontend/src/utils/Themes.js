e1564d0702384dee50cc1f774fdb82a3dc0389b2
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-20 18:42:21 +0100

    Create Themes.js

---
+++ frontend/src/utils/Themes.js
@@ -0,0 +1,23 @@
+export const lightTheme = {
+    primary: '#76dd67',
+    secondary: '#f9f9ef',
+    text_primary: '#1c1c1c',
+    text_secondary: '#272727',
+    card: '#fbfbf5',
+    card_light: '#fffff9',
+    card_dark: '#f7f7f1',
+    black: '#000',
+    white: '#fff',
+};
+
+export const darkTheme = {
+    primary: '#76dd56',
+    secondary: '#272727',
+    text_primary: '#fffffa',
+    text_secondary: '#f9f9ef',
+    card: '#29292d',
+    card_light: '#2d2d31',
+    card_dark: '#252529',
+    black: '#000',
+    white: '#fff',
+};
