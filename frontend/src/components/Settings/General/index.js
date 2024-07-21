aee5dc6b57f5162abfeb6035c98b7d047bd10cb3
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 17:14:58 +0200

    Update index.js

--- frontend/src/components/Settings/General/index.js
+++ frontend/src/components/Settings/General/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function General() {
     return (
         <div>
-        <h1>General Settings</h1>
+        <h1 style={{height: '5000px'}}>General Settings</h1>
         </div>
     );
 }
