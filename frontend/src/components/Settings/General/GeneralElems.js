9c94bd8b5d7f6d00e88ec782a1425036dc0670ff
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 19:23:36 +0200

    Update GeneralElems.js

--- frontend/src/components/Settings/General/GeneralElems.js
+++ frontend/src/components/Settings/General/GeneralElems.js
@@ -25,21 +25,97 @@ export const Title = styled.div`
   margin: 20px 0px;
 `;
 
-export const section = styled.div`
+export const Section = styled.div`
   display: flex;
-  width: 90%;
+  width: 60%;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
   margin-bottom: 20px;
   border-radius: 20px;
   background-color: ${({ theme }) => theme.primary + 20};
+  @media screen and (max-width: 768px) {
+    width: 70%;
+  }
+  @media screen and (max-width: 640px) {
+    width: 80%;
+  }
 `;
 
-export const sectionTitle = styled.div`
+export const SectionTitle = styled.div`
   font-size: 24px;
   font-weight: 500;
   color: ${({ theme }) => theme.text_primary};
   margin: 10px 0px;
   align-self: center;
+`;
+
+export const Field = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 32px;
+`;
+
+export const FieldTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const FieldTitle = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+export const FieldValContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FieldVal = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+export const FieldInput = styled.input`
+  font-size: 16px;
+  width: 60%;
+  font-weight: 400;
+  text-align: center;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  border-radius: 10px;
+  padding: 4px 8px;
+  margin-left: 24px;
+`;
+
+export const FieldButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
+`;
+
+export const FieldButton = styled.button`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.card_dark};
+  background-color: ${({ theme }) => theme.primary};
+  border: none;
+  border-radius: 10px;
+  border: 2px solid ${({ theme }) => theme.primary};
+  cursor: pointer;
+  padding: 4px 8px;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: ${({ theme }) => theme.secondary};
+    color: ${({ theme }) => theme.primary};
+  }
 `;
