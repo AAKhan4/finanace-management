7777da58dc5f2cc749e188d52a6bcff88ee487ef
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 17:56:37 +0200

    Update index.js

--- frontend/src/components/Settings/General/index.js
+++ frontend/src/components/Settings/General/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import * as E from "./GeneralElems";
 
 export default function General() {
-    return (
-        <div>
-        <h1 style={{height: '5000px'}}>General Settings</h1>
-        </div>
-    );
+  return (
+    <E.Container>
+      <E.Wrapper>
+        <E.Title>General</E.Title>
+        <E.section>
+          <E.sectionTitle>Account</E.sectionTitle>
+        </E.section>
+        <E.section>
+          <E.sectionTitle>Password</E.sectionTitle>
+        </E.section>
+        <E.section>
+          <E.sectionTitle>Appearance</E.sectionTitle>
+        </E.section>
+      </E.Wrapper>
+    </E.Container>
+  );
 }
