e855150faeb44d61c4a121a28a437012cf9f5748
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 13:58:30 +0200

    Update Nav

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as E from "./NavElems";
+import { UserContext } from "../../context/UserContext";
 
 export default function Nav() {
+  const { user } = useContext(UserContext);
   return (
     <E.Container>
       <E.Wrapper>
-        <E.Logo href="/"><E.Name>FinMan</E.Name></E.Logo>
+        <E.Logo href="/">
+          <E.Name>FinMan</E.Name>
+        </E.Logo>
         <E.ButtonContainer>
-          <E.Button href="/">Home</E.Button>
-          <E.Button href="/login">Login</E.Button>
-          <E.Button href="/signup">Signup</E.Button>
+          {user ? (
+            <>
+              <E.User href="/settings">{user.username}</E.User>
+              <E.Button href="/logout">Logout</E.Button>
+            </>
+          ) : (
+            <>
+              <E.Button href="/login">Login</E.Button>
+              <E.Button href="/signup">Signup</E.Button>
+            </>
+          )}
         </E.ButtonContainer>
       </E.Wrapper>
     </E.Container>
