84a56d19cf8256176dbdca1ff8897afceea49829
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 17:05:58 +0200

    Update index.js

--- frontend/src/components/Settings/index.js
+++ frontend/src/components/Settings/index.js
@@ -1,4 +1,15 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import General from './General';
-import * as E from './SettingsElems';
+import React from "react";
+import Navbar from "./Navbar";
+import General from "./General";
+import * as E from "./SettingsElems";
+
+export default function Settings() {
+  return (
+    <E.Container>
+      <E.Wrapper>
+          <Navbar />
+          <General />
+      </E.Wrapper>
+    </E.Container>
+  );
+}
