ccd56f1a37f797ad7b61c3104f3f2198ce31589a
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-22 01:07:40 +0200

    Update index.js

--- frontend/src/components/Settings/General/index.js
+++ frontend/src/components/Settings/General/index.js
@@ -1,27 +1,18 @@
 import React, { useContext, useState } from "react";
 import * as E from "./GeneralElems";
+import { UserContext } from "../../../context/UserContext";
+import Accounts from "./Accounts";
 
 export default function General() {
-  const [username, setUsername] = useState("demo");
-  const [email, setEmail] = useState("demo@example.com");
-  const [userUpdate, setUserUpdate] = useState("");
-  const [emailUpdate, setEmailUpdate] = useState("");
+  const { user, setToken } = useContext(UserContext);
+
   return (
     <E.Container>
       <E.Wrapper>
         <E.Title>General</E.Title>
         <E.Section>
           <E.SectionTitle>Account</E.SectionTitle>
-          <E.Field>
-            <E.FieldTitle>Username</E.FieldTitle>
-            <E.FieldVal>{username}</E.FieldVal>
-            <E.FieldButton>Update</E.FieldButton>
-          </E.Field>
-          <E.Field>
-            <E.FieldTitle>Email</E.FieldTitle>
-            <E.FieldInput></E.FieldInput>
-            <E.FieldButton>Update</E.FieldButton>
-          </E.Field>
+          <Accounts />
         </E.Section>
         <E.Section>
           <E.SectionTitle>Password</E.SectionTitle>
