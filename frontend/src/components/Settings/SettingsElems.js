469cd2242d61e9452d3f891b61c50cfbf758421b
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 17:05:55 +0200

    Update SettingsElems.js

--- frontend/src/components/Settings/SettingsElems.js
+++ frontend/src/components/Settings/SettingsElems.js
@@ -1,17 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
   background-color: ${({ theme }) => theme.secondary};
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  top: 60px;
+  padding: 0px 0px;
 `;
 
 export const Wrapper = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  width: 100%;
+  max-width: 1400px;
 `;
+
+
