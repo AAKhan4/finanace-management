a3d3ded06e3a76f27391a73b612ab459daeea0d5
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 18:50:16 +0200

    Update user.js

--- backend/routes/userRoutes/user.js
+++ backend/routes/userRoutes/user.js
@@ -32,6 +32,11 @@ router.delete("/", (req, res) => {
 router.patch("/", (req, res) => {
   if (!req.cookies.user) return res.status(401).send("You are not logged in");
 
+  if (req.body.password)
+    return res
+      .status(400)
+      .send("Password cannot be updated through this route");
+
   const user = User.findByIdAndUpdate(req.cookies.user, req.body, {
     new: true,
     runValidators: true,
@@ -39,6 +44,30 @@ router.patch("/", (req, res) => {
   if (!user) return res.status(404).send("User not found");
 
   res.status(200).send("User updated successfully");
+});
+
+router.patch("/password", (req, res) => {
+  if (!req.cookies.user) return res.status(401).send("You are not logged in");
+
+  if (!(req.body.password && req.body.newPassword))
+    return res.status(400).send("Password & new Password is required");
+
+  const user = User.findById(req.cookies.user);
+  if (!user) return res.status(404).send("User not found");
+
+  if (!bcrypt.compare(req.body.password, user.password))
+    return res.status(401).send("Invalid password");
+
+  User.findByIdAndUpdate(
+    req.cookies.user,
+    { password: req.body.newPassword },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).send("Password updated successfully");
 });
 
 module.exports = router;
