be919610535f22d458f47b97bcb53a458ef8751b
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 17:32:16 +0200

    Update NavElems.js

--- frontend/src/components/Nav/NavElems.js
+++ frontend/src/components/Nav/NavElems.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.primary};
   width: 100%;
-  height: 80px;
+  height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
