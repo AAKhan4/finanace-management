c96974d6a5684d9d761efa67b5e5fd32f5f217a7
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-21 19:23:38 +0200

    Update index.js

--- frontend/src/components/Settings/General/index.js
+++ frontend/src/components/Settings/General/index.js
@@ -6,15 +6,25 @@ export default function General() {
     <E.Container>
       <E.Wrapper>
         <E.Title>General</E.Title>
-        <E.section>
-          <E.sectionTitle>Account</E.sectionTitle>
-        </E.section>
-        <E.section>
-          <E.sectionTitle>Password</E.sectionTitle>
-        </E.section>
-        <E.section>
-          <E.sectionTitle>Appearance</E.sectionTitle>
-        </E.section>
+        <E.Section>
+          <E.SectionTitle>Account</E.SectionTitle>
+          <E.Field>
+            <E.FieldTitle>Username</E.FieldTitle>
+            <E.FieldVal>demo</E.FieldVal>
+            <E.FieldButton>Update</E.FieldButton>
+          </E.Field>
+          <E.Field>
+            <E.FieldTitle>Email</E.FieldTitle>
+            <E.FieldInput></E.FieldInput>
+            <E.FieldButton>Update</E.FieldButton>
+          </E.Field>
+        </E.Section>
+        <E.Section>
+          <E.SectionTitle>Password</E.SectionTitle>
+        </E.Section>
+        <E.Section>
+          <E.SectionTitle>Appearance</E.SectionTitle>
+        </E.Section>
       </E.Wrapper>
     </E.Container>
   );
