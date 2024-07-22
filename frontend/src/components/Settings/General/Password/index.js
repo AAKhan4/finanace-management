70a73a04c4016bd581555a0e1f1a336704e6d135
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-22 20:12:37 +0200

    Update Password

--- frontend/src/components/Settings/General/Password/index.js
+++ frontend/src/components/Settings/General/Password/index.js
@@ -8,40 +8,51 @@ export default function Password() {
   };
   return (
     <>
-      <E.Field>
-        <E.FieldTitleContainer>
-          <E.FieldTitle>Current Password</E.FieldTitle>
-        </E.FieldTitleContainer>
-        <E.FieldValContainer style={{ width: "60%" }}>
-          <E.FieldInput type="password" name="password" />
-        </E.FieldValContainer>
-      </E.Field>
-      <E.Field>
-        <E.FieldTitleContainer>
-          <E.FieldTitle>New Password</E.FieldTitle>
-        </E.FieldTitleContainer>
-        <E.FieldValContainer style={{ width: "60%" }}>
-          <E.FieldInput type="password" name="newPassword" />
-        </E.FieldValContainer>
-      </E.Field>
-      <E.Field>
-        <E.FieldTitleContainer>
-          <E.FieldTitle>Confirm Password</E.FieldTitle>
-        </E.FieldTitleContainer>
-        <E.FieldValContainer style={{ width: "60%" }}>
-          <E.FieldInput type="password" name="confirmPassword" />
-        </E.FieldValContainer>
-      </E.Field>
-      <E.Field style={{ justifyContent: "center", marginTop: "40px" }}>
-        {toggle ? (
-          <E.FieldButtonContainer>
-            <E.Accept>Confirm</E.Accept>
-            <E.Cancel onClick={handleToggle}>Cancel</E.Cancel>
-          </E.FieldButtonContainer>
-        ) : (
-          <E.FieldButton onClick={handleToggle}>Change Password</E.FieldButton>
-        )}
-      </E.Field>
+      {toggle ? (
+        <>
+          <E.Field>
+            <E.FieldTitleContainer>
+              <E.FieldTitle>Current Password</E.FieldTitle>
+            </E.FieldTitleContainer>
+            <E.FieldValContainer style={{ width: "60%" }}>
+              <E.FieldInput type="password" name="password" />
+            </E.FieldValContainer>
+          </E.Field>
+          <E.Field>
+            <E.FieldTitleContainer>
+              <E.FieldTitle>New Password</E.FieldTitle>
+            </E.FieldTitleContainer>
+            <E.FieldValContainer style={{ width: "60%" }}>
+              <E.FieldInput type="password" name="newPassword" />
+            </E.FieldValContainer>
+          </E.Field>
+          <E.Field>
+            <E.FieldTitleContainer>
+              <E.FieldTitle>Confirm Password</E.FieldTitle>
+            </E.FieldTitleContainer>
+            <E.FieldValContainer style={{ width: "60%" }}>
+              <E.FieldInput type="password" name="confirmPassword" />
+            </E.FieldValContainer>
+          </E.Field>
+          <E.Field style={{ justifyContent: "center", marginTop: "40px" }}>
+            <E.FieldButtonContainer>
+              <E.Accept>Confirm</E.Accept>
+              <E.Cancel onClick={handleToggle}>Cancel</E.Cancel>
+            </E.FieldButtonContainer>
+          </E.Field>
+        </>
+      ) : (
+        <>
+          <E.SectionSubtitle style={{ textAlign: "center" }}>
+            Account may be lost if password is forgotten
+          </E.SectionSubtitle>
+          <E.Field style={{ justifyContent: "center", marginTop: "40px" }}>
+            <E.FieldButton onClick={handleToggle}>
+              Change Password
+            </E.FieldButton>
+          </E.Field>
+        </>
+      )}
     </>
   );
 }
