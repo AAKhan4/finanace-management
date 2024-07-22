a373cab43b605e0dbf224e69d23fa5741cd8133b
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-22 04:36:12 +0200

    Update NavbarElems.js

--- frontend/src/components/Settings/Navbar/NavbarElems.js
+++ frontend/src/components/Settings/Navbar/NavbarElems.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.card_dark};
+  justify-content: center;
   width: 100%;
   height: 50px;
   display: flex;
@@ -9,10 +10,8 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   position: sticky;
-  top: 60px;
   padding: 0px 20px;
   z-index: 99;
-  border-bottom: 1px solid ${({ theme }) => theme.primary};
   @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
   }
@@ -26,7 +25,7 @@ export const Wrapper = styled.div`
   max-width: 1400px;
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 export const Option = styled.a`
@@ -36,13 +35,14 @@ export const Option = styled.a`
   width: 100%;
   color: ${({ theme }) => theme.text_secondary};
   padding: 8px 16px;
-  border-radius: 10px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
   cursor: pointer;
   ${({ active, theme }) =>
     active &&
     `
-        color: ${theme.card_dark};
-        background-color: ${theme.primary + 80};
+      color: ${theme.card_dark};
+      background-color: ${theme.primary+99};
     `}
   text-decoration: none;
   transition: all 0.2s ease-in-out;
@@ -51,11 +51,11 @@ export const Option = styled.a`
     morgin-top: 0px;
   }
   &:hover {
+    color: ${({ theme }) => theme.text_primary};
     ${({ active, theme }) =>
       !active &&
       `
-        color: ${theme.text_primary};
-        background-color: ${theme.primary + 30};
+        background-color: ${theme.primary + 40};
     `}
   }
 `;
