f1e67daa5febe175dd874233822b7ae670452d48
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-22 16:53:56 +0200

    Update Password

--- frontend/src/components/Settings/General/Password/index.js
+++ frontend/src/components/Settings/General/Password/index.js
@@ -2,18 +2,46 @@ import React from "react";
 import * as E from "../GeneralElems";
 
 export default function Password() {
+  const [toggle, setToggle] = React.useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
   return (
-    <E.Field>
-      <E.FieldTitleContainer>
-        <E.FieldTitle>CurrentPassword</E.FieldTitle>
-      </E.FieldTitleContainer>
-      <E.FieldValContainer>
-        <E.FieldInput
-          type="password"
-          name="password"
-          style={{textAlign: "left"}}
-        />
-      </E.FieldValContainer>
-    </E.Field>
+    <>
+      <E.Field>
+        <E.FieldTitleContainer>
+          <E.FieldTitle>Current Password</E.FieldTitle>
+        </E.FieldTitleContainer>
+        <E.FieldValContainer style={{ width: "60%" }}>
+          <E.FieldInput type="password" name="password" />
+        </E.FieldValContainer>
+      </E.Field>
+      <E.Field>
+        <E.FieldTitleContainer>
+          <E.FieldTitle>New Password</E.FieldTitle>
+        </E.FieldTitleContainer>
+        <E.FieldValContainer style={{ width: "60%" }}>
+          <E.FieldInput type="password" name="newPassword" />
+        </E.FieldValContainer>
+      </E.Field>
+      <E.Field>
+        <E.FieldTitleContainer>
+          <E.FieldTitle>Confirm Password</E.FieldTitle>
+        </E.FieldTitleContainer>
+        <E.FieldValContainer style={{ width: "60%" }}>
+          <E.FieldInput type="password" name="confirmPassword" />
+        </E.FieldValContainer>
+      </E.Field>
+      <E.Field style={{ justifyContent: "center", marginTop: "40px" }}>
+        {toggle ? (
+          <E.FieldButtonContainer>
+            <E.Accept>Confirm</E.Accept>
+            <E.Cancel onClick={handleToggle}>Cancel</E.Cancel>
+          </E.FieldButtonContainer>
+        ) : (
+          <E.FieldButton onClick={handleToggle}>Change Password</E.FieldButton>
+        )}
+      </E.Field>
+    </>
   );
 }
