d2fa5a3fe19b900a9b2ff9a6cc3b657a9c051522
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-22 04:36:21 +0200

    Update General Settings

--- frontend/src/components/Settings/General/index.js
+++ frontend/src/components/Settings/General/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import * as E from "./GeneralElems";
-import { UserContext } from "../../../context/UserContext";
 import Accounts from "./Accounts";
 
 export default function General() {
   return (
     <E.Container>
       <E.Wrapper>
-        <E.Title>General</E.Title>
+        <E.Title>General Settings</E.Title>
         <E.Section>
           <E.SectionTitle>Account</E.SectionTitle>
           <Accounts />
