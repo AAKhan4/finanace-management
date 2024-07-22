865551daaf4f045b14a4964fc7d3d481cf677cc2
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-22 20:28:28 +0200

    Update Password

--- frontend/src/components/Settings/General/Password/index.js
+++ frontend/src/components/Settings/General/Password/index.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import * as E from "../GeneralElems";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { UserContext } from "../../../../context/UserContext";
 
 export default function Password() {
-  const [toggle, setToggle] = React.useState(false);
+  const { setToken } = useContext(UserContext);
+  const path = "http://localhost:5050/user/password";
+  const [toggle, setToggle] = useState(false);
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const handleToggle = () => {
     setToggle(!toggle);
   };
+
+  const handlePasswordSubmit = () => {
+    if (newPassword !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    if (newPassword.length < 8) {
+      alert("Password must be at least 8 characters long");
+      return;
+    }
+
+    const payload = {
+      password: password,
+      newPassword: newPassword,
+    };
+
+    axios
+      .patch(path, payload)
+      .then((res) => {
+        console.log(res);
+        setToken(null);
+        setToken(Cookies.get("user"));
+        setToggle(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       {toggle ? (
@@ -15,7 +53,13 @@ export default function Password() {
               <E.FieldTitle>Current Password</E.FieldTitle>
             </E.FieldTitleContainer>
             <E.FieldValContainer style={{ width: "60%" }}>
-              <E.FieldInput type="password" name="password" />
+              <E.FieldInput
+                type="password"
+                name="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
             </E.FieldValContainer>
           </E.Field>
           <E.Field>
@@ -23,7 +67,13 @@ export default function Password() {
               <E.FieldTitle>New Password</E.FieldTitle>
             </E.FieldTitleContainer>
             <E.FieldValContainer style={{ width: "60%" }}>
-              <E.FieldInput type="password" name="newPassword" />
+              <E.FieldInput
+                type="password"
+                name="newPassword"
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              />
             </E.FieldValContainer>
           </E.Field>
           <E.Field>
@@ -31,12 +81,18 @@ export default function Password() {
               <E.FieldTitle>Confirm Password</E.FieldTitle>
             </E.FieldTitleContainer>
             <E.FieldValContainer style={{ width: "60%" }}>
-              <E.FieldInput type="password" name="confirmPassword" />
+              <E.FieldInput
+                type="password"
+                name="confirmPassword"
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+              />
             </E.FieldValContainer>
           </E.Field>
           <E.Field style={{ justifyContent: "center", marginTop: "40px" }}>
             <E.FieldButtonContainer>
-              <E.Accept>Confirm</E.Accept>
+              <E.Accept onClick={handlePasswordSubmit}>Confirm</E.Accept>
               <E.Cancel onClick={handleToggle}>Cancel</E.Cancel>
             </E.FieldButtonContainer>
           </E.Field>
