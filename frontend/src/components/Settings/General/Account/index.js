31211ee95e835bb0b7be6e05bded6b78ebb7ad76
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-23 02:58:53 +0200

    Rename General/Accounts to  Account

---
+++ frontend/src/components/Settings/General/Account/index.js
@@ -0,0 +1,114 @@
+import React, { useState, useContext } from "react";
+import * as E from "../GeneralElems";
+import axios from "axios";
+import { UserContext } from "../../../../context/UserContext";
+import Cookies from "js-cookie";
+
+export default function Accounts() {
+  const path = "http://localhost:5050/user";
+  const { user } = useContext(UserContext);
+  let demoUser = user;
+  if (!demoUser)
+    demoUser = {
+      username: "demo",
+      email: "demo@example.com",
+    };
+
+  const { setToken } = useContext(UserContext);
+  const [userToggle, setUserToggle] = useState(false);
+  const [name, setName] = useState(demoUser.username);
+  const [emailToggle, setEmailToggle] = useState(false);
+  const [email, setEmail] = useState(demoUser.email);
+
+  const handleUserToggle = () => {
+    setUserToggle(!userToggle);
+  };
+
+  const handleUserSubmit = () => {
+    axios
+      .patch(path, { username: name })
+      .then((res) => {
+        console.log(res);
+        setToken(null);
+        setToken(Cookies.get("user"));
+        setUserToggle(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleEmailToggle = () => {
+    setEmailToggle(!emailToggle);
+  };
+
+  const handleEmailSubmit = () => {
+    axios
+      .patch(path, { email: email })
+      .then((res) => {
+        console.log(res);
+        setToken(null);
+        setToken(Cookies.get("user"));
+        setEmailToggle(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const renderAccountsFields = () => {
+    return ["u", "e"].map((field) => {
+      const toggle = field === "u" ? userToggle : emailToggle;
+      const handleToggle = field === "u" ? handleUserToggle : handleEmailToggle;
+      const handleSubmit = field === "u" ? handleUserSubmit : handleEmailSubmit;
+
+      return (
+        <E.Field key={field}>
+          <E.FieldTitleContainer>
+            <E.FieldTitle>{field === "u" ? "Username" : "Email"}</E.FieldTitle>
+          </E.FieldTitleContainer>
+          {toggle ? (
+            <>
+              <E.FieldValContainer>
+                <E.FieldInput
+                  onChange={(e) => {
+                    field === "u"
+                      ? setName(e.target.value)
+                      : setEmail(e.target.value);
+                  }}
+                  placeholder={
+                    field === "u" ? demoUser.username : demoUser.email
+                  }
+                  defaultValue={
+                    field === "u" ? demoUser.username : demoUser.email
+                  }
+                />
+              </E.FieldValContainer>
+              <E.FieldButtonContainer>
+                <E.Accept onClick={handleSubmit}>Y</E.Accept>
+                <E.Cancel onClick={handleToggle}>N</E.Cancel>
+              </E.FieldButtonContainer>
+            </>
+          ) : (
+            <>
+              <E.FieldVal>
+                {field === "u" ? demoUser.username : demoUser.email}
+              </E.FieldVal>
+              <E.FieldButtonContainer>
+                <E.FieldButton onClick={handleToggle}>Update</E.FieldButton>
+              </E.FieldButtonContainer>
+            </>
+          )}
+        </E.Field>
+      );
+    });
+  };
+  return (
+    <>
+      <E.SectionSubtitle style={{ textAlign: "center" }}>
+        User Details & Information
+      </E.SectionSubtitle>
+      {renderAccountsFields()}
+    </>
+  );
+}
