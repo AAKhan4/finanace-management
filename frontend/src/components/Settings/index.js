36089c0f61a7dbb9f36a4b0d61a9a0d3bd8c5492
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-07-24 02:41:49 +0200

    Update Settings

--- frontend/src/components/Settings/index.js
+++ frontend/src/components/Settings/index.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import General from "./General";
 import * as E from "./SettingsElems";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 
 export default function Settings() {
   const demoMode = true;
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
-  if (!demoMode) {
+  if (!(demoMode && user)) {
     navigate("/login");
   }
 
   return (
     <E.Container>
       <E.Wrapper>
-          <Navbar />
-          <General />
+        <Navbar />
+        <General />
       </E.Wrapper>
     </E.Container>
   );
