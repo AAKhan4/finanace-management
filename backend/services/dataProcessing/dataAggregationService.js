935f16508d2095d0f4698807aa5b54067493de8a
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-08-12 12:34:19 +0200

    Create dataAggregationService.js

---
+++ backend/services/dataProcessing/dataAggregationService.js
@@ -0,0 +1,106 @@
+const Transaction = require("../../schema/TransactionSchema");
+const Budget = require("../../schema/BudgetSchema");
+
+exports.getSpendingsByCategory = async (id, startDate, endDate) => {
+  return await Transaction.aggregate([
+    {
+      $match: {
+        user: id,
+        date: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$category",
+        total: { $sum: "$amount" },
+      },
+    },
+    {
+      $lookup: {
+        from: "categories",
+        localField: "_id",
+        foreignField: "_id",
+        as: "category",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        category: "$category.name",
+        total: 1,
+      },
+    },
+  ]);
+};
+
+exports.getSpendingsByWallet = async (id, startDate, endDate) => {
+  return await Transaction.aggregate([
+    {
+      $match: {
+        user: id,
+        date: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$wallet",
+        total: { $sum: "$amount" },
+      },
+    },
+    {
+      $lookup: {
+        from: "wallets",
+        localField: "_id",
+        foreignField: "_id",
+        as: "wallet",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        wallet: "$wallet.name",
+        total: 1,
+      },
+    },
+  ]);
+};
+
+exports.getSpendingOverTime = async (id, startDate, endDate, interval) => {
+  return await Transaction.aggregate([
+    {
+      $match: {
+        user: id,
+        date: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          day: { $dayOfMonth: "$date" },
+          week: { $week: "$date" },
+          month: { $month: "$date" },
+          year: { $year: "$date" },
+        }[interval],
+        total: { $sum: "$amount" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        date: "$_id",
+        total: 1,
+      },
+    },
+  ]);
+};
+
+
